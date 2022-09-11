4eb6ba89538782ce99eedf276f9f31fab2e52d69
HoangAnhKy <hoanganhkyltt@gmail.com>
2022-09-12 01:00:42 +0700

    update commit

--- NodeJS/thuchanh/src/index.js
+++ NodeJS/thuchanh/src/index.js
@@ -2,16 +2,19 @@
 const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, 'public'));
 app.engine(
-    'hds',
+    'hbs',
     handlebars.engine({
-        extname: '.hds',
+        extname: '.hbs',
     })
 );
-app.set('view engine', 'hds');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
     res.render('home');
