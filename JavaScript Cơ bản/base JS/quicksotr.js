5e36d18d3ad0bbf354a3cfe95bdc5dc0ddcf3f26
HoangAnhKy <hoanganhkyltt@gmail.com>
2023-10-29 15:53:35 +0700

    add sortMerge

--- JavaScript Cơ bản/base JS/quicksotr.js
+++ JavaScript Cơ bản/base JS/quicksotr.js
@@ -18,3 +18,28 @@ const quickSort = (arr) => {
     }
     return [...quickSort(left), pivot, ...quickSort(right)];
 };
+
+// merge sort
+function sort_merge(arr = []) {
+    if (arr.length < 2){
+        return arr;
+    }
+    let right = [...arr];
+    let mid = right.length / 2;
+    let left = right.splice(0, mid);
+    return merge(sort_merge(left), sort_merge(right));
+}
+
+function merge(left, right) {
+    let arr_sort = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr_sort.push(left.shift())
+        } else {
+            arr_sort.push(right.shift())
+        }
+    }
+
+    return [...arr_sort, ...left, ...right];
+}
