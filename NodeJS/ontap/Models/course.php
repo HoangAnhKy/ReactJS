664bd601cdbabe509e82d28fb4b0bf76f6db71ba
Hoàng Anh Kỳ <90885739+HoangAnhKy@users.noreply.github.com>
2024-05-17 13:03:27 +0700

    Delete NodeJS/ontap/Models directory

--- NodeJS/ontap/Models/course.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $connect = mysqli_connect('127.0.0.1', 'root', '', 'school');
-    $sql = "SELECT * FROM courses";
-    $data = mysqli_query($connect, $sql);
-    echo json_encode($data -> fetch_all(MYSQLI_ASSOC));
-?>
