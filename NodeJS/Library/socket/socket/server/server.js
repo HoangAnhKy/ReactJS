0db3045972a61a3e1facc11b0fd780e930b7769f
Hoàng Anh Kỳ <90885739+HoangAnhKy@users.noreply.github.com>
2024-08-06 11:18:37 +0700

    upload demo

--- NodeJS/Library/socket/socket/server/server.js
+++ NodeJS/Library/socket/socket/server/server.js
@@ -5,27 +5,36 @@ let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 
 //redirect / to our index.html file
-app.get('/', function(req, res,next) {  
+app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/users', function(req, res,next) {  
+app.get('/users', function(req, res,next) {
     res.sendFile(__dirname + '/public/users.html');
 });
+
+app.get('/users2', function(req, res,next) {
+    res.sendFile(__dirname + '/public/users2.html');
+});
+
 let list_chat = [];
 //when a client connects, do this
 io.on('connection', function(client) {
     console.log('Client connected...');
     //when the server receives clicked message, do this
-    client.on('room_1', function(data) {
+    client.on('send_message', function(data) {
         list_chat.push(data)
         //send a message to ALL connected clients
-        io.emit('room_reps', list_chat);
+        client.join(data.room);
+        io.to(data.room).emit('room_reps', list_chat);
+    });
+    client.on('leave', function(data) {
+        client.leave(data.room);
     });
 });
 
 
 //start our web server and socket.io server listening
 server.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
